d73e39336acb312c036cde4068b7c11fdeb2dde0
Michael Bujol <bujolm@gmail.com>
2016-05-05 13:02:01 -0500

    add node_path (for external programs using node); update editorconfig

--- roles/zsh/_path.zsh
+++ roles/zsh/_path.zsh
@@ -2,5 +2,5 @@
 # Make sure to APPEND to the path, so that homebrewed binaries, e.g. /usr/local/bin are found first
 
 export GOPATH=$HOME/src/go
-
+export NODE_PATH=/usr/local/lib/node_modules
 export PATH=$HOME/bin:$PATH:$ZSH/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin:/usr/texbin:$GOPATH/bin
